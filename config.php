ac4e61a4912168c3cf44f073558b65589875ec12
xHaileab <xHaileab@gmail.com>
2024-03-01 14:22:25 -0800

    Limtless FirstPush

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 $db_host = 'localhost'; // MySQL host (usually localhost)
 $db_user = 'root'; // MySQL username
-$db_pass = 'CbUng+S5bvWhRnaX0GE92gFjM43Q99YYM8Jrk9OApTBrJJrWfRdh2cUJGVlAbwYt2U617hPPZFMQC7Pl127YwA=='; // MySQL password
+$db_pass = 'CbUng+S5bvWhRnaX0GE92gFjM43Q99YYM8JbwYt2U617hPPZFMQC7Pl127YwA=='; // MySQL password
 $db_name = 'limitless'; // Database name
 
 
